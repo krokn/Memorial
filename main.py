1ea325df931de608f53aac9e1202a71e4f82b288
Your Name <you@example.com>
2024-10-08 14:15:26 +0300

    add CORS

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = FastAPI(
     title='Хеллоу Мария'
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 for router in all_routers:
     app.include_router(router)
