f8d8d4a392c157fd27066fd128f69d88f3182b9d
Vlad <dalvvlad2004@mail.ru>
2025-01-21 15:25:30 +0300

    save

--- src/response/post.py
+++ src/response/post.py
@@ -6,17 +6,17 @@ from src.database.models import PostModel, EducationInstitutionModel
 async def allPostsJson(posts):
     posts_dict = [
         {
-            'title': post[0].title,
-            'content': post[0].content,
-            'linkToPhoto': post[0].linkToPhoto,
-            'address': post[0].address,
-            'x': post[0].x,
-            'y': post[0].y,
+            'title': post.title,  # Доступ к post.title из PostModel
+            'content': post.content,
+            'linkToPhoto': post.linkToPhoto,
+            'address': post.address,
+            'x': post.x,
+            'y': post.y,
             'user': {
-                'name': post[1],
-                'surname': post[2],
-                'fatherName': post[3],
-                'educationInstitution': post[4]
+                'name': post.user.name,  # Доступ к user.name из UserModel
+                'surname': post.user.surname,
+                'fatherName': post.user.fatherName,
+                'educationInstitution': post.user.EducationInstitution.name  # Алиас из запроса
             }
         }
         for post in posts
